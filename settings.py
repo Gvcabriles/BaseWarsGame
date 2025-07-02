126ed311e710d5816fd5470a5cc66f5505f2f64e
Gerard Cabriles <gerard.cabriles@student.curtin.edu.au>
2025-07-02 18:23:30 +0800

    Created 'settings.py'. Colour propertiese and screen properties added

---
+++ settings.py
@@ -0,0 +1,15 @@
+import pygame
+
+#Colour Properties 
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+YELLOW = (255, 255, 0)
+LIGHTGRAY = (200, 200, 200)
+DARKGRAY = (150, 150, 150)
+RED = (255, 0 , 0)
+
+#Screen
+DISP_WIDTH = 1200
+DISP_HEIGHT = 800
+FPS = 60
