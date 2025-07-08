a7330159887f924cb781d89f16285a1be26cf8d3
Gerard Cabriles <gerard.cabriles@student.curtin.edu.au>
2025-07-08 23:55:37 +0800

    Updated file.

--- sounds.py
+++ sounds.py
@@ -2,16 +2,15 @@ import pygame
 import os
 
 pygame.mixer.init()
+pygame.mixer.music.load(os.path.join("sounds", "bgmusic.wav"))
 
-#Background Music
-bg_music = pygame.mixer.music.load(os.path.join("sounds", "bgmusic.wav"))
-#menu_music = pygame.mixer.music.load(os.path.join("sounds", "menu_music.wav"))
-
-#Shoot Sound Effects
-SHOOT_SOUND = pygame.mixer.Sound(os.path.join("sounds","shootsound.wav"))
-
-#Sound Effects 
-DEATHs   = pygame.mixer.Sound(os.path.join("sounds", "deathsound.wav"))
+SHOOT_SOUND = pygame.mixer.Sound(os.path.join("sounds", "shootsound.wav"))
+DEATH_SOUND = pygame.mixer.Sound(os.path.join("sounds", "deathsound.wav"))
 HIT_SOUND = pygame.mixer.Sound(os.path.join("sounds", "hitsound.wav"))
 DROID_DEATH = pygame.mixer.Sound(os.path.join("sounds", "droiddeath.wav"))
-deathaudio = pygame.mixer.Sound(os.path.join("sounds", "defeatsound.wav"))
+DEATH_AUDIO = pygame.mixer.Sound(os.path.join("sounds", "defeatsound.wav"))
+
+DROID_DEATH.set_volume(0.3)
+DEATH_SOUND.set_volume(0.5)
+SHOOT_SOUND.set_volume(0.3)
+DEATH_AUDIO.set_volume(0.1)
