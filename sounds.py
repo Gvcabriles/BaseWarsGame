3011c433fde3bb585a1fea2b240d8199fd8800db
Gerard Cabriles <gerard.cabriles@student.curtin.edu.au>
2025-07-08 23:52:08 +0800

    Updated variables

--- sounds.py
+++ sounds.py
@@ -8,10 +8,10 @@ bg_music = pygame.mixer.music.load(os.path.join("sounds", "bgmusic.wav"))
 #menu_music = pygame.mixer.music.load(os.path.join("sounds", "menu_music.wav"))
 
 #Shoot Sound Effects
-shoot_sound = pygame.mixer.Sound(os.path.join("sounds","shootsound.wav"))
+SHOOT_SOUND = pygame.mixer.Sound(os.path.join("sounds","shootsound.wav"))
 
 #Sound Effects 
-death_sound = pygame.mixer.Sound(os.path.join("sounds", "deathsound.wav"))
-hitsound = pygame.mixer.Sound(os.path.join("sounds", "hitsound.wav"))
-droid_death = pygame.mixer.Sound(os.path.join("sounds", "droiddeath.wav"))
+DEATHs   = pygame.mixer.Sound(os.path.join("sounds", "deathsound.wav"))
+HIT_SOUND = pygame.mixer.Sound(os.path.join("sounds", "hitsound.wav"))
+DROID_DEATH = pygame.mixer.Sound(os.path.join("sounds", "droiddeath.wav"))
 deathaudio = pygame.mixer.Sound(os.path.join("sounds", "defeatsound.wav"))
